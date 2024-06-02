77925a36e5dde6e72c7a2b3a294bb9dca1d3e027
Yevheniia-narizhna <162164893+Yevheniia-narizhna@users.noreply.github.com>
2024-06-02 19:02:39 +0300

    first

---
+++ js/task-3.js
@@ -0,0 +1,37 @@
+const profile = {
+  username: "Jacob",
+  playTime: 300,
+};
+
+changeUsername(newName)
+for (const name of username) {
+    username = newName;
+}
+updatePlayTime(hours)
+for (const name of username) {
+    playTime += hours;
+}
+
+getInfo()
+return <Username> has <amount> active hours!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
